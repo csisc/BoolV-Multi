750a2c3c33b8ec0334767f64623433055b83d6ca
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2024-12-08 14:16:47 +0100

    Add files via upload

---
+++ gpu_optimize.sh
@@ -0,0 +1,4 @@
+pip install --no-cache-dir llama-cpp-python==0.2.90 --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu123
+#Substitute pandas by fireducks.pandas
+pip install fireducks
+wget https://huggingface.co/lmstudio-community/Llama-3.2-3B-Instruct-GGUF/resolve/main/Llama-3.2-3B-Instruct-Q8_0.gguf
